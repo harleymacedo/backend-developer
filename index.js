ad237a2e541d167ed594b57db7301387cc9e8788
harleymacedo <harley.ip@gmail.com>
2021-07-14 22:15:16 -0300

    Primeira rota criada

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000 
 
-app.listen( process.env.PORT || 3000 )
+app.get('/', (req, res) => {
+    res.status(200).send({
+        'mensagem': 'Primeira rota com GET'
+    })
+})
+
+app.listen(port, () => {console.log(`App rodando em http://localhost:${port}`)})
